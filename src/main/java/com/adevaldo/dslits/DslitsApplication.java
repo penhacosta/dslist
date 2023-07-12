c58c3521c2747fb95e3e0e3e903fa574f1ecbb29
ADEVALDO <35281765+penhacosta@users.noreply.github.com>
2023-07-12 16:55:11 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/adevaldo/dslits/DslitsApplication.java
@@ -0,0 +1,13 @@
+package com.adevaldo.dslits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adevaldo/dslits/DslitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adevaldo.dslits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
