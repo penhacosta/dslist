1ed6bcb71979667d037bd86223cf668a68e17a33
ADEVALDO <35281765+penhacosta@users.noreply.github.com>
2023-07-15 17:38:21 -0300

    Homolog Ok

--- src/main/java/com/adevaldo/dslits/projections/GameMinProjection.java
+++ src/main/java/com/adevaldo/dslits/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.adevaldo.dslits.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
